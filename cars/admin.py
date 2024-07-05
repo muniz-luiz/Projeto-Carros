cdac73e494164a34a0b4fa873d81dc931d9ce33b
muniz-luiz <eusouluizeduardomuniz@gmail.com>
2024-07-05 15:15:13 -0300

    Adicionando no administrador

--- cars/admin.py
+++ cars/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from cars.models import Car
 
-# Register your models here.
+class CarAdmin(admin.ModelAdmin):
+  list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
+  search_fields = ('model',)
+
+  
+admin.site.register(Car, CarAdmin)
