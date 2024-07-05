b8d0ffe0993c8756bfcb5a19b383a77794107b30
muniz-luiz <eusouluizeduardomuniz@gmail.com>
2024-07-05 14:04:33 -0300

    Criando a classe Car

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+  id = models.AutoField(primary_key=True)
+  model = models.CharField(max_length=200)
+  brand = models.CharField(max_length=200)
+  factory_year = models.IntegerField(blank=True, null=True)
+  model_year = models.IntegerField(blank=True, null=True)
+  value = models.FloatField(blank=True, null=True)
+
+
+
+
+
+
