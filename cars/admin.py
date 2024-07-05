70f0ea8b7d71040b1e6341d40f7cf86515f8acbf
muniz-luiz <eusouluizeduardomuniz@gmail.com>
2024-07-05 16:10:42 -0300

    Criando o campo imagens e adicionando o pillow

--- cars/admin.py
+++ cars/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from cars.models import Car
+from cars.models import Car, Brand
+
 
 class CarAdmin(admin.ModelAdmin):
   list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
   search_fields = ('model', 'brand')
 
-  
+class BrandAdmin(admin.ModelAdmin):
+  list_display = ('name',)
+  search_fields = ('name',)
+
+admin.site.register(Brand, BrandAdmin)
 admin.site.register(Car, CarAdmin)
--- cars/models.py
+++ cars/models.py
@@ -7,14 +7,15 @@ class Brand(models.Model):
   def __str__(self):
     return self.name
 
-
 class Car(models.Model):
   id = models.AutoField(primary_key=True)
   model = models.CharField(max_length=200)
   brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand')
   factory_year = models.IntegerField(blank=True, null=True)
   model_year = models.IntegerField(blank=True, null=True)
+  plate = models.CharField(blank=True, null=True, max_length=10)
   value = models.FloatField(blank=True, null=True)
+  photo = models.ImageField(upload_to='cars/', blank=True, null=True)
 
 
   def __str__(self):
