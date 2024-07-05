e5b28d2a0f7e3f0ced884dd073852283c49600ac
muniz-luiz <eusouluizeduardomuniz@gmail.com>
2024-07-05 15:46:40 -0300

    Criando o campo marca

--- cars/models.py
+++ cars/models.py
@@ -1,15 +1,24 @@
 from django.db import models
 
+class Brand(models.Model):
+  id = models.AutoField(primary_key=True)
+  name = models.CharField(max_length=200)
+
+  def __str__(self):
+    return self.name
+
+
 class Car(models.Model):
   id = models.AutoField(primary_key=True)
   model = models.CharField(max_length=200)
-  brand = models.CharField(max_length=200)
+  brand = models.ForeignKey(Brand, on_delete=models.PROTECT, related_name='car_brand')
   factory_year = models.IntegerField(blank=True, null=True)
   model_year = models.IntegerField(blank=True, null=True)
   value = models.FloatField(blank=True, null=True)
 
 
-
+  def __str__(self):
+    return self.model
 
 
 
