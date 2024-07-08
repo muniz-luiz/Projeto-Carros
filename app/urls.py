b84bcfe47038f58e0743a39ed82c4e00c4886591
muniz-luiz <eusouluizeduardomuniz@gmail.com>
2024-07-08 09:19:14 -0300

    Fazendo na url

--- app/urls.py
+++ app/urls.py
@@ -18,8 +18,13 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from cars.views import cars_view
+
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+    path('cars/', cars_view),
+        
 ] + static(settings.MEDIA_URL, documents_root=settings.MEDIA_ROOT)
--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def cars_view(request):
+  return HttpResponse('Carros')
